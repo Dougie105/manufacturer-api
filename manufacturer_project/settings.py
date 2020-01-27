db9203df7d43b933aa4697e3f5fe4205245aa8cc
Terrell Douglas <terrelloydouglas@yahoo.com>
2020-01-27 10:21:39 -0800

    added gitignore

--- manufacturer_project/settings.py
+++ manufacturer_project/settings.py
@@ -140,6 +140,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_in_env')]
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
 STATICFILES_DIRS = [
     STATIC_DIR,
